ec5df2cdd0b7e53a1be109cb0fa2069533edb760
Teklu Mezgebo <mezge002@umn.edu>
2023-01-20 14:18:13 -0600

    commit tree 8511600c7cb756742624e123989d6206ef2efb39

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+  else if (someValue > 400 && someValue < 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue > 2000 && someValue < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (someValue > 2500) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(somecity){
+  const city = 'NYC'
+  return somecity === city ? ( 'Ok, sounds good.') : ('No go.')
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default: 
+    return 'Bye.'
+  }
 }
